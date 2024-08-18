ccbcf4deec35665cec818ca7f6974ba6ca03adc9
Agustinus Juan Kurniawan <juankurniawan04@gmail.com>
2024-08-18 19:19:13 +0700

    authorization implemented

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
-
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 
+const ClientError = require('./exceptions/ClientError');
 // notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
-
 // users
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
-
 // authentications
 const authentications = require('./api/authentications');
 const AuthenticationsService = require('./services/postgres/AuthenticationsService');
-const TokenManager = require('./tokenize/TokenManager');
 const AuthenticationsValidator = require('./validator/authentications');
+const TokenManager = require('./tokenize/TokenManager');
 
 const init = async () => {
     const notesService = new NotesService();
@@ -83,6 +82,33 @@ const init = async () => {
             },
         },
     ]);
+
+    server.ext('onPreResponse', (request, h) => {
+        // mendapatkan konteks response dari request
+        const { response } = request;
+
+        if (response instanceof ClientError) {
+            const clientErrorResponse = h.response({
+                status: 'fail',
+                message: response.message,
+            });
+            clientErrorResponse.code(response.statusCode);
+            return clientErrorResponse;
+        }
+
+        if (response.isServer) {
+            // Server ERROR!
+            const serverErrorResponse = h.response({
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.',
+            });
+            serverErrorResponse.code(serverErrorResponse.statusCode);
+            return serverErrorResponse;
+        }
+
+        // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+        return response.continue || response;
+    });
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
